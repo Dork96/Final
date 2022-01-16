9811038de590f5c4f98842988eb4f7e9697255c2
amingnurfalah <48075172+Dork96@users.noreply.github.com>
2022-01-16 22:37:50 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -224,7 +224,7 @@ echo '.....done'
 echo; echo 'Installation has completed.'
 echo 'Config file is at /usr/local/ddos/ddos.conf'
 echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
-
+clear
 # banner /etc/issue.net
 wget -q -O /etc/issue.net "https://raw.githubusercontent.com/Dork96/Final/main/banner.conf"
 echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
