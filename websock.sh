e30aebabfc0a4f45439897f4b948d9323b435c36
amingnurfalah <48075172+Dork96@users.noreply.github.com>
2022-01-24 00:38:14 +0700

    Update websock.sh

--- websock.sh
+++ websock.sh
@@ -108,7 +108,7 @@ wget https://github.com/ambrop72/badvpn/archive/master.zip
 unzip master.zip
 cd badvpn-master
 mkdir build
-cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
+cmake . -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
 sudo make install
 
 # Install Module
